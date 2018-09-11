06bc41d5313650ca23fc5a491d7a94a83683eb18
Kaan Taha Koken <kaan_taha_koken@hotmail.com>
2018-09-11 17:58:06 +0300

    new route added (userData), user route updated

--- api/route/user.js
+++ api/route/user.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const checkAuth = require('../middleware/check-auth')
 const UserControllers =  require('../controllers/user');
 
 router.post('/signup', UserControllers.user_signup);
@@ -9,5 +8,5 @@ router.post('/login', UserControllers.user_login);
 
 router.get('/', UserControllers.user_get_all);
 
-router.delete('/:userId', checkAuth, UserControllers.user_delete_user);
+router.delete('/:userId', UserControllers.user_delete_user);
 module.exports = router;
