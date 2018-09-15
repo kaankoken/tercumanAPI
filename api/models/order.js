b982abe72bf6b74dfdf2db6b3c417e2912ddb08f
Kaan Taha Koken <kaan_taha_koken@hotmail.com>
2018-09-15 22:23:49 +0300

    orders', products' & userdatas' model updated

--- api/models/order.js
+++ api/models/order.js
@@ -2,8 +2,8 @@ const mongoose =  require('mongoose');
 
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    wordCount: { type: Number, default: 1 },
+    product: { type: mongoose.Schema.Types.Mixed, ref: 'Product', required: true },
+    wordCount: { type: Number, default: 1 }, //dosya açma ve kelime saydırmaya bakılacak
     totalPrice: {type: Number, default: 1}
 });
 
--- api/models/product.js
+++ api/models/product.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const productSchema =  mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    sourceLanguage: { type: String, require: true },
-    destinationLanguage: { type: String, require: true},
-    unitPrice: { type: Number, require: true },
+    srcLang: { type: mongoose.Schema.Types.Mixed, ref: 'Language', require: true },
+    destLang: { type: mongoose.Schema.Types.Mixed, ref: 'Language', require: true},
+    translationPrice: { type: Number },
     file: { type: String, require: true },
 });
 
