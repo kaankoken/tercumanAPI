37b540bf207c0908d840f42e10129a1b5e4c1ab6
Kaan Taha Koken <kaan_taha_koken@hotmail.com>
2018-09-16 13:27:58 +0300

    userData is no longer needed

--- api/models/userData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DataModel = mongoose.Schema({
-    _id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true},
-    order: { type: mongoose.Schema.Types.ObjectId, ref: 'Order'},
-});
-
-module.exports = mongoose.model('userDataModel', DataModel);
