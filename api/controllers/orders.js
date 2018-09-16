9d7f7ba315ff90c33a84e360970d59910fb1bfc2
Kaan Taha Koken <kaan_taha_koken@hotmail.com>
2018-09-17 01:18:41 +0300

    user model added, necessary depencies & functions added for file reading to controllers

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const Order = require('../models/order');
 const Product  = require('../models/product');
+const mammoth =  require('mammoth');
+var pdfUtil = require('pdf-to-text');
+const fs = require('fs');
 
 exports.orders_get_all = (req, res, next) => {
     Order.find()
         .select('_id product wordCount totalPrice')
         .populate('product')
+        .populate('user')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -13,6 +17,7 @@ exports.orders_get_all = (req, res, next) => {
                 orders: docs.map(doc => {
                     return {
                         _id: doc._id,
+                        user: doc.user,
                         product: doc.product,
                         wordCount: doc.wordCount,
                         totalPrice: doc.totalPrice,
@@ -39,12 +44,35 @@ exports.orders_create_order =  (req, res, next) => {
                     message: 'Product Not Found'
                 });
             }
-            console.log(product.translationPrice    );
+            if (product.file.search('.docx') != -1) {
+                mammoth.extractRawText({path: __rootdir + '/public/uploads/' + product.file})
+                    .then(result => {
+                        var text = result.value;
+                        text = text.split(' ').length;
+                        console.log(text);
+                    })
+                    .done();
+            } else if (product.file.search('.txt') != -1) {
+                var text = fs.readFileSync(__rootdir + '/public/uploads/' + product.file, 'utf-8');
+                text = text.split(' ').length;
+                console.log(text);
+            } else if (product.file.search('.pdf')) {
+                pdfUtil.pdfToText(__rootdir + '/public/uploads/' + product.file, function(err, text) {
+                    if (err) throw(err);
+                    text = text.split(' ').length;
+                    console.log(text);
+                });
+            } else {
+                res.status(500).json({
+                    error: 'unidentifed file'
+                });
+            }
             const order =  new Order({
                 _id: new mongoose.Types.ObjectId(),
+                user: req.body.userId,
                 product: req.body.productId,
-                wordCount: req.body.wordCount,
-                totalPrice: req.body.wordCount * product.translationPrice
+                wordCount: 0,
+                totalPrice: 0 * product.translationPrice
             });
             return order.save();    
         })
@@ -54,6 +82,7 @@ exports.orders_create_order =  (req, res, next) => {
                 message: 'Order stored',
                 createdOrder: {
                     _id: result._id,
+                    user: result.user,
                     product: result.product,
                     wordCount: result.wordCount,
                     totalPrice: result.totalPrice
@@ -75,6 +104,7 @@ exports.orders_create_order =  (req, res, next) => {
 exports.orders_get_order = (req, res, next) => {
     Order.findById(req.params.orderId)
         .populate('product')
+        .populate('user')
         .exec()
         .then(order => {
             if (!order ) {
--- api/models/order.js
+++ api/models/order.js
@@ -3,8 +3,9 @@ const mongoose =  require('mongoose');
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     product: { type: mongoose.Schema.Types.Mixed, ref: 'Product', required: true },
-    wordCount: { type: Number, default: 1 }, //dosya açma ve kelime saydırmaya bakılacak
-    totalPrice: {type: Number, default: 1}
+    wordCount: { type: Number, default: 1 },
+    totalPrice: {type: Number, default: 1},
+    user: { type: mongoose.Schema.Types.Mixed, ref: 'User', required: true}
 });
 
 module.exports = mongoose.model('Order', orderSchema);
